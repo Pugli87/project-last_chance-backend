e740323a648b4a9cad8d2e42d68aae7fec463fda
Julian-363 <juliangil333@gmail.com>
2023-12-09 19:21:37 -0500

    controler logout

--- controllers/users/logoutUser.js
+++ controllers/users/logoutUser.js
@@ -1,3 +1,20 @@
-const logout = async (req, res) => {console.log("route");}
-module.exports = { logout, }
-// 
+// controllers/authController.js
+//const login = require("../../service/users");
+const User=require("../../models/userSchema")
+const logout = async (req, res) => {
+  try {
+    console.log("controlers");
+    const user = await User.findOne({ _id: require.user._id });
+    if (!user) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+    user.token = ""; // Delete the token on the user
+    await user.save();
+    res.status(204).send(); // Successful response without content
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+module.exports = {
+  logout,
+};
