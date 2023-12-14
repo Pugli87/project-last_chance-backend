a42c2c1216d3559f30fa50e780fd8b27cf55cd86
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-12-14 02:01:04 -0500

    service logout pendiente funcionamiento

--- service/users/index.js
+++ service/users/index.js
@@ -2,10 +2,12 @@ const createUser = require("./createUser");
 const { login } = require("./loginUser");
 const verifyUserEmail = require("./verifyUserEmail");
 const verifyUser = require("./verifyUser");
+const { logout } = require("./logoutUser");
 
 module.exports = {
-  createUser,
-  login,
-  verifyUserEmail,
-  verifyUser,
+	createUser,
+	login,
+	verifyUserEmail,
+	verifyUser,
+	logout,
 };
--- service/users/logoutUser.js
+++ service/users/logoutUser.js
@@ -1,14 +1,25 @@
-const logout = async (req, res) => {
+const logout = async (_id) => {
+	try {
+		await User.findOneAndUpdate(
+			{ _id },
+			{ $set: { token: null } },
+			{ new: true }
+		);
 
-  try {
-    return console.log("entra");
- } catch (error) {
-    // Si hay algún error, imprímelo en la consola y devuelve un objeto con éxito falso y mensaje de error
-    console.error('Error al cerrar sesión:', error);
-    return { success: false, message: 'Error al cerrar sesión' };
-  }
+		return {
+			success: true,
+			result: {},
+			message: "",
+		};
+	} catch (error) {
+		return {
+			success: false,
+			result: null,
+			message: error,
+		};
+	}
 };
 
 module.exports = {
-  logout,
+	logout,
 };
