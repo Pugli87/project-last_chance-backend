d1b197ced34efab1b456bdfae8f6dedbcbb7e9d0
miguelangelacosta <113574101+miguelangelacosta@users.noreply.github.com>
2023-12-17 21:48:37 -0500

    lisNotFoodprivate

--- routes/caloriesprivate.routes.js
+++ routes/caloriesprivate.routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const caloriesPrivate = require("../controllers/caloriesprivate/caloriesprivate");
 const auth = require("../middleware/auth"); 
 
-
 router.get("/daily-rate/lisNotFoodpriv", auth, caloriesPrivate.getDailyIntakeAndNonRecFoods);
 
 module.exports = router;
