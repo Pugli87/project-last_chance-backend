bf511f14d685987f8df75ca557dea38831e37a2b
miguelangelacosta <113574101+miguelangelacosta@users.noreply.github.com>
2023-12-17 21:17:34 -0500

    lisNotFoodpriva

--- routes/calorias.routes.js
+++ routes/calorias.routes.js
@@ -1,11 +1,12 @@
+
 const express = require("express");
 const router = express.Router();
 const controllers = require("../controllers/calorias/recommendedCaloriescontroller");
 
-// Route to get daily calorie intake
-router.get("/dailyintake/:userId", controllers.getDailyCaloricIntake);
+// Ruta para obtener la ingesta diaria de calorías
+router.get("/daily-rate/:userId", controllers.getDailyCaloricIntake);
 
-// Route to get the list of non-recommended foods
-router.get("/nonrecommendedfoods", controllers.getNonRecommendedFoods);
+// Ruta para obtener la lista de alimentos no recomendados
+router.get("/listNotFood", controllers.getNonRecommendedFoods);
 
 module.exports = router;
--- routes/caloriesprivate.routes.js
+++ routes/caloriesprivate.routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const caloriesPrivate = require("../caloriesprivate");
+const caloriesPrivate = require("../controllers/caloriesprivate/caloriesprivate");
 const auth = require("../middleware/auth"); // Importa el middleware de autenticación
 
 // Ruta protegida que requiere autenticación
-router.get("/getDailyIntakeAndNonRecFoods", auth, caloriesPrivate.getDailyIntakeAndNonRecFoods);
+router.get("/lisNotFoodpriv", auth, caloriesPrivate.getDailyIntakeAndNonRecFoods);
 
 module.exports = router;
